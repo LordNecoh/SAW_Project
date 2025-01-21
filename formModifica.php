0fa04c1172862cb6a93ffe781cc92bbf8ffab5be
LordNecoh <leonardonecordi@gmail.com>
2025-01-21 17:38:13 +0100

    Messo a posto index, spostato css modifica profilo

--- formModifica.php
+++ formModifica.php
@@ -4,129 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/body.css">
     <link rel="stylesheet" href="css/formModifica.css">
     <title>User Profile</title>
-    <style>
-        .container {
-            display: flex;
-            align-items: flex-start;
-            flex-direction: column;
-            justify-content: center;
-            height: auto;
-            margin-top: 20px;
-            gap: 20px;
-        }
-
-        .user-icon {
-            width: 20%;
-            text-align: center;
-            padding: 20px;
-            position: relative;
-        }
-
-        .user-icon img {
-            border-radius: 50%;
-            width: 150px;
-            transition: transform 0.3s ease, box-shadow 0.3s ease;
-        }
-
-        .user-icon img.editable {
-            cursor: pointer;
-        }
-
-        .user-icon img.editable:hover {
-            transform: scale(1.1);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
-
-        .form-container,
-        .password-form-container {
-            width: 60%;
-            background: #ffffff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .form-row {
-            display: flex;
-            gap: 10px;
-            margin-bottom: 15px;
-        }
-
-        .form-control,
-        .password-form-control {
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            font-size: 14px;
-        }
-
-        .form-control.half-width {
-            flex: 1;
-        }
-
-        .form-control.large-width {
-            flex: 2;
-        }
-
-        .form-button {
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-            display: inline-block;
-        }
-
-        .form-button:hover {
-            background-color: #0056b3;
-        }
-
-        .form-button.hidden {
-            display: none;
-        }
-
-        .cancel-button {
-            background-color: #dc3545;
-        }
-
-        .cancel-button:hover {
-            background-color: #c82333;
-        }
-
-        .confirm-button {
-            background-color: #28a745;
-        }
-
-        .confirm-button:hover {
-            background-color: #218838;
-        }
-
-        h2 {
-            margin-bottom: 20px;
-        }
-
-        .hidden {
-            display: none;
-        }
-    </style>
     <script src="js/gestioneFormModifica.js"></script>
     </head>
-
 <body>
-
     <?php require 'header.php';
     require 'database/get_info.php';
     ?>
 
     <div class="container">
         <div class="user-icon">
-            <img id="profileImage"
-                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-                alt="User Icon">
+            <img src="images/profilePicture.png" alt="User Icon" id="userIcon">
         </div>
         <div class="form-container">
             <h2>User Information</h2>
--- index.php
+++ index.php
@@ -10,23 +10,27 @@
 </head>
 <body>
     <?php include 'header.php'?>
-    
 
     
-    <div id="mainBody" class="TextBody" >
-        <h1> Welcome to Chuck the Beaver's very unofficial website! <h1>
-        <p>Chuck the Beaver is an exciting tower defense game created for the GMTK GameJam 2024.<br><br> 
-            The theme for this year's jam is "built to scale," and Chuck the Beaver embodies this concept perfectly.<br><br> 
-            Players must strategically place and upgrade their defenses to protect their territory from waves of enemies.<br><br> 
-            As the game progresses, the challenges scale up, requiring players to adapt and enhance their strategies to succeed.<br><br>
-            Join Chuck the Beaver in this thrilling adventure and see if you have what it takes to defend your Dam!</p>
-        <!-- <img src="images/kBsiWa.jpg" alt="Chuck the Beaver"> -->
-        <p>In the world of Chuck the Beaver, our protagonist is an industrious beaver who is obsessed with upgrading his dam.<br><br> 
-            His relentless pursuit of perfection and efficiency has led him to block an important river, disrupting the natural balance of the forest.<br><br> 
-            As a result, the forest creatures, angered by the changes and the threat to their habitat, have revolted against Chuck.<br><br> 
-            They are determined to destroy his dam and restore the river to its former glory.<br><br>
-            Players must help Chuck defend his dam from the onslaught of forest creatures, using strategy and upgrades to withstand the relentless attacks.</p>
+    <h1> Welcome to Chuck the Beaver's very unofficial website! </h1>
+    <div class="baseText">
+        Chuck the Beaver is a very exciting tower defense game created for the GMTK GameJam 2024.<br> 
+        The theme for this year's jam is "built to scale," and Chuck the Beaver embodies this concept perfectly.<br>
+        Players must strategically place and upgrade their defenses to protect their territory from waves of enemies.<br>
+        As the game progresses, the challenges scale up, requiring players to adapt and enhance their strategies to succeed.<br>
+        Join Chuck the Beaver in this thrilling adventure and see if you have what it takes to defend your Dam!
     </div>
+    <br>
+    <img src="images/gameImage.png" alt="Chuck the Beaver" class="centeredImage">
+
+    <div class="baseText">
+        In the world of Chuck the Beaver, our protagonist is an industrious beaver who is obsessed with upgrading his dam.<br>
+        His relentless pursuit of perfection and efficiency has led him to block an important river, disrupting the natural balance of the forest.<br>
+        As a result, the forest creatures, angered by the changes and the threat to their habitat, have revolted against Chuck.<br>
+        They are determined to destroy his dam and restore the river to its former glory.<br>
+        Players must help Chuck defend his dam from the onslaught of forest creatures, using strategy and upgrades to withstand the relentless attacks.
+    </div>
+        
 
 </body>
 </html>
