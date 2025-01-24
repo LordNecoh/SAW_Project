e4d23e9b6e53eb9d4a7d8c7c4b6cf1df26db9882
LordNecoh <leonardonecordi@gmail.com>
2025-01-24 21:19:32 +0100

    Fatta query primo comando admin

--- database/adminActions.php
+++ database/adminActions.php
@@ -7,7 +7,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     switch ($action) {
         case 'topDonors':
             $topN = $_POST['topN'];
-            // Logica per cercare i top N donatori
+            try{
+                $Query = $conn->query("SELECT u.username, d.email, SUM(d.amount) AS total_donated
+                                    FROM donations d
+                                    INNER JOIN users u ON u.email = d.email
+                                    WHERE d.public = 1
+                                    GROUP BY u.username, d.email
+                                    ORDER BY total_donated DESC
+                                    LIMIT $topN");
+                $donors = $Query->fetchAll();
+                echo json_encode([
+                    'success' => true,
+                    'donors' => $donors
+                ]);
+            } catch (PDOException $e) {
+                echo json_encode([
+                    'success' => false,
+                    'error' => $e->getMessage()
+                ]);
+                exit;
+            }
+
+            //Da continuare
             break;
 
         case 'userDonations':
