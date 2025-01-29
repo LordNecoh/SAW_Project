e17ca5f2ff89bc1603a8008dbb1de4bb5c1a3de8
LordNecoh <leonardonecordi@gmail.com>
2025-01-29 14:46:33 +0100

    popup a lato e controllo extra admin

--- database/adminActions.php
+++ database/adminActions.php
@@ -1,5 +1,13 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    session_start();
+    if (!isset($_SESSION['admin'])) {
+        echo json_encode([
+            'success' => false,
+            'error' => 'Unauthorized access'
+        ]);
+        exit;
+    }
     require 'connessioneDB.php';
 
     $action = $_POST['action'];
